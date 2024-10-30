24ecd063f700f9c97e44a4d1c0e0ae37ffa5f6df
thangkhungtheki <thangkhungtheki@gmail.com>
2024-10-30 15:57:21 +0700

    l

--- layouts/layout-hrm.vue
+++ layouts/layout-hrm.vue
@@ -34,7 +34,7 @@
         <li class="nav-item" >
           <p class="nav-link bg-light">
             <i class="fa fa-th-large mr-3 text-primary fa-fw " />
-            <nuxt-link to="/index">Bảng Tổng</nuxt-link>
+            <nuxt-link to="/xemluong">Xem Lương</nuxt-link>
           </p>
         </li>
         <!-- <li class="nav-item">
